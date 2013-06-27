96312c4ee55aece1dd6046ca9c1f8661644b9de7
H. Rene Trevino <rtrevinojr@utexas.edu>
2013-06-26 22:06:24 -0500

    Unit Test cases for add_root method: Creates a master root on the tree for parsing.

--- TestXML.py
+++ TestXML.py
@@ -16,7 +16,9 @@ import xml.etree.ElementTree as ET
 import StringIO
 import unittest
 
-from XML import is_nested, xml_search
+# from XML import is_nested_it, xml_search_2
+# from xml_static import is_nested, xml_search
+from XML import XML
 
 #
 # TestXML
@@ -47,6 +49,9 @@ class TestXML(unittest.TestCase) :
 	query2 = " <Team> </Team> "
 	query3 = " <Team> <Cooly> <Amber> </Amber> </Cooly> </Team> "
 	
+	# global XML Object
+	xml_obj = XML('RunXML.in')
+	
 	
 	'''
 	# Constructor
@@ -66,7 +71,8 @@ class TestXML(unittest.TestCase) :
 		eroot = etree.getroot()
 		fquery = StringIO.StringIO(self.query)
 		qtree = ET.parse(fquery)
-		result = is_nested(etree, qtree)
+		#
+		result = self.xml_obj.is_nested_it(etree, qtree)
 		self.assert_(result == True)
 		
 	def test_is_nested_2(self) :
@@ -75,7 +81,8 @@ class TestXML(unittest.TestCase) :
 		eroot = etree.getroot()
 		fquery = StringIO.StringIO(self.query2)
 		qtree = ET.parse(fquery)
-		result = is_nested(etree, qtree)
+		#
+		result = self.xml_obj.is_nested_it(etree, qtree)
 		self.assert_(result == True)
 		
 	def test_is_nested_3(self) :
@@ -84,7 +91,8 @@ class TestXML(unittest.TestCase) :
 		eroot = etree.getroot()
 		fquery = StringIO.StringIO(self.query2)
 		qtree = ET.parse(fquery)
-		result = is_nested(etree, qtree)
+		#
+		result = self.xml_obj.is_nested_it(etree, qtree)
 		self.assert_(result == True)
 		
 	#
@@ -96,7 +104,8 @@ class TestXML(unittest.TestCase) :
 		eroot = etree.getroot()
 		fquery = StringIO.StringIO(self.query)
 		qtree = ET.parse(fquery)
-		result = xml_search(etree, qtree)
+		#
+		result = self.xml_obj.xml_search(etree, qtree)
 		self.assert_(result == [2,[2,7]])
 		
 	def test_xml_search_2(self) :
@@ -105,7 +114,8 @@ class TestXML(unittest.TestCase) :
 		eroot = etree.getroot()
 		fquery = StringIO.StringIO(self.query)
 		qtree = ET.parse(fquery)
-		result = xml_search(etree, qtree)
+		#
+		result = self.xml_obj.xml_search(etree, qtree)
 		self.assert_(result == [2,[2, 7]])
 		
 	def test_xml_search_3(self) :
@@ -114,8 +124,28 @@ class TestXML(unittest.TestCase) :
 		eroot = etree.getroot()
 		fquery = StringIO.StringIO(self.query3)
 		qtree = ET.parse(fquery)
-		result = xml_search(etree, qtree)
+		#
+		result = self.xml_obj.xml_search(etree, qtree)
 		self.assert_(result == [1,[7]])
+		
+	#
+	# test_add_root()
+	#
+	def test_add_root(self) :
+		self.xml_obj.add_root('RunXML.in', 'RunXML.in')
+		master_tree = ET.parse('RunXML.in')
+		mroot = master_tree.getroot()
+		self.assert_(mroot.tag == "ROOT")
+		
+	def test_add_root_2(self) :
+		self.xml_obj.add_root('RunXML.in', 'RunXML.in')
+		master_tree = ET.parse('RunXML.in')
+		mroot = master_tree.getroot()
+		self.assert_(mroot.tag == "ROOT")
+		
+		
+		
+		
 		
 			
 		
