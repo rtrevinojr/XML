3cfe7c35e137be205b6edc867599e5ede94adb79
H. Rene Trevino <rtrevinojr@utexas.edu>
2013-06-26 21:08:02 -0500

    XML Object class: Constructor will parse the xml file.

--- XML.py
+++ XML.py
@@ -2,62 +2,98 @@
 import xml.etree.ElementTree as ET
 
 
-def xml_tree() :
-	
-	etree = ET.parse('RunXML.in')
-	root = etree.getroot()
-	
-	print type(etree)
-	# assert type(etree) == xml.etree.ElementTree
-	
-	print "\n"
-	print "root: ", root
-	print "root.tag - ", root.tag
-	print "root.attrib - ", root.attrib, "\n"
-	
-	st = "<Team> <Cooly> </Cooly> </Team>"
-	qtree = ET.parse('query.in')
-	print "qtree: ", qtree, "\n"
-	
-	ET.dump(qtree)
-	
-	index = 0
-	edict = {}
-	
-	for v in etree.iter() :
-		print v
-		index += 1
-		edict[index] = v
-	
-	print "\n"
-	print edict
-
-	ET.dump(root)
+# q: parameter object & self
+class XML(object) :
 
 
 
-#
-# is_nested_it function: Returns count for qtree nesting occurances
-#
-def is_nested_it(etree, qtree) :
+    def __init__(self, xmlfile) :
+        """
 
-    etreeit = etree.iter()
-    qtreeit = qtree.iter()
+        :param xmlfile:
+        """
+        self.add_root(xmlfile, 'mainRunXML.in')
+        self.maintree = ET.parse('mainRunXML.in')
+        self.mainroot = self.maintree.getroot()
+        self.root_lst = self.mainroot.getchildren()
+        self.etree = ET.parse('mainRunXML.in')
+        self.etree_list = []
 
-    qtit = qtreeit.next()
-    nest = True
-    nest_cnt = 0
+        #self.qtree = qt
 
-    try :
-        for i in etreeit :
-            if i.tag == qtit.tag :
-                qtit = qtreeit.next()
-                nest_cnt += 1
-                print "nest_cnt - ", nest_cnt
-    except StopIteration :
-        return True
-    else :
-        return False
+    def is_nested_it(self, etree, qtree) :
+
+        eroot = etree.getroot()
+        qroot = qtree.getroot()
+
+        etreeit = etree.iter()
+        qtreeit = qtree.iter()
+
+        qtit = qtreeit.next()
+        nest = True
+
+        try :
+            for i in etreeit :
+
+                if i.tag == qtit.tag :
+                    qtit = qtreeit.next()
+        except StopIteration :
+            return True
+        else :
+            return False
+
+
+
+    def xml_search(self, etrees, qtrees) :
+        #assert type(et) is types.ET
+
+        count = 0
+        linecnt = 0
+        line = []
+        #qt = qtrees.iter()
+
+        try :
+            for i in etrees.iter() :
+                linecnt += 1
+                qt = qtrees.iter()
+                if qtrees.getroot().tag == i.tag :
+                    etrees._setroot(i)
+                    iq = qt.next()
+                    qtrees._setroot(iq)
+                    if self.is_nested_it(etrees, qtrees) :
+                        count += 1
+                        line += [linecnt]
+        except StopIteration :
+            pass
+
+        result = []
+        result.append(count)
+        result.append(line)
+        return result
+
+    def add_root(self, rfile, wfile) :
+        assert type(rfile) is str
+        assert type(wfile) is str
+        r = open(rfile, 'r')
+        assert type(r) is file
+        xml_file = r.read()
+        r.close()
+        w = open(wfile, 'w')
+        assert type(w) is file
+        w.write("<ROOT> " + "\n" )
+        w.write(xml_file)
+        #w.write( "\n" )
+        w.write( "</ROOT>")
+        w.close()
+
+
+    def parse_trees(root_xml) :
+        assert type(root_xml) is str
+        maintree = ET.parse(root_xml)
+        main_root = maintree.getroot()
+        docs = main_root.getchildren()
+        return docs
+
 
 def is_nested_it_2(etree, qtree) :
 
@@ -99,8 +135,7 @@ def is_nested_it_2(etree, qtree) :
     return nest_cnt
 
 
-
-def xml_search(etrees, qtrees) :
+def xml_search_2(etrees, qtrees) :
     #assert type(et) is types.ET
     count = 0
     linecnt = 0
@@ -111,12 +146,16 @@ def xml_search(etrees, qtrees) :
             linecnt += 1
             qt = qtrees.iter()
             if qtrees.getroot().tag == i.tag :
+
                 etrees._setroot(i)
                 iq = qt.next()
                 qtrees._setroot(iq)
-                if is_nested_it(etrees, qtrees) :
-                    count += 1
-                    line += [linecnt]
+                nest_cnt = is_nested_it_2(etrees, qtrees)
+                if nest_cnt > 0 :
+                    count += nest_cnt
+                    while nest_cnt != 0 :
+                        line += [linecnt]
+                        nest_cnt -= 1
     except StopIteration :
         pass
 
