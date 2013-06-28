3ad6fb1b41a75f5d9bb2b62bf8042bd0a87ced52
H. Rene Trevino <rtrevinojr@utexas.edu>
2013-06-28 18:30:19 -0500

    Final commit to turnin.

--- XML.py
+++ XML.py
@@ -8,13 +8,22 @@ class XML(object) :
 
 
     def __init__(self, xmlfile) :
+        """
+        XML Constructor: Reads input file and will parse RunXML.in by adding a master root then convert back into an Element Tree.
+        """
         #assert type(xmlfile) is str
         self.add_root(xmlfile, 'masterRunXML.in')
         self.maintree = ET.parse('masterRunXML.in')
         self.mainroot = self.maintree.getroot()
         self.root_lst = self.mainroot.getchildren()
 
+
+
     def add_root(self, rfile, wfile) :
+        """
+        Read rfile and writes to wfile with a master root for parsing
+        """
+
         w = open(wfile, 'w')
         w.write("<ROOT> " + "\n" )
         for v in rfile :
@@ -22,8 +31,11 @@ class XML(object) :
         w.write( "</ROOT>")
         w.close()
 
-    def is_nested(self, etree, qtree) :
 
+    def is_nested(self, etree, qtree) :
+        """
+        Returns a count for the number of times qtree is nested in etree
+        """
         temp_etree = etree
         temp_qt = qtree
 
@@ -36,12 +48,15 @@ class XML(object) :
         try :
             for i in etreeit :
                 if i.tag == next_qtreeit.tag :
+                    #next_qtreeit = qtreeit.next()
                     temp_etree._setroot(i)
                     temp_etreeit = temp_etree.iter()
+
                     temp_qtit = temp_qt.iter()
                     next_temp_qtit = temp_qtit.next()
-                    
+
                     for i2 in temp_etreeit :
+
                         try :
                             if i2.tag == next_temp_qtit.tag :
                                 next_temp_qtit = temp_qtit.next()
@@ -58,6 +73,9 @@ class XML(object) :
 
 
     def xml_search(self, etrees, qtrees) :
+        """
+        Method will iterate through etrees and return a list with a count and list of occurences where qtrees exist
+        """
         #assert type(etrees) is ET
         count = 0
         linecnt = 0
@@ -85,15 +103,23 @@ class XML(object) :
         result.append(line)
         return result
 
+
 def xml_print(w, l) :
+    """
+    Function will output the result of xml_search the way Sphere desires
+    """
     w.write( str(l[0]) + "\n" )
     l = l[1]
     for i in l :
         w.write( str(i) + "\n" )
     w.write("\n")
 
-def xml_run(xmlin, out) :
 
+
+def xml_run(xmlin, out) :
+    """
+    Function will use the XML constructor to parse RunXML.in into a list of root nodes and output the results of the program.
+    """
     xmlobj = XML(xmlin)
     rlist = xmlobj.root_lst
 
